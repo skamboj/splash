ddb91a748e78568afce56d416b7ad876ae6ee0a1
Sachin Kamboj <sachin.kamboj@gmail.com>
2011-06-04 20:17:34 -0400

    Added auto_increment fields

--- splash.sql
+++ splash.sql
@@ -1,136 +1,109 @@
--- MySQL dump 10.13  Distrib 5.1.38, for apple-darwin9.5.0 (i386)
+-- phpMyAdmin SQL Dump
+-- version 2.11.11.3
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: splash
--- ------------------------------------------------------
--- Server version	5.1.38
+-- Host: 184.168.194.11
+-- Generation Time: Jun 04, 2011 at 06:15 PM
+-- Server version: 5.0.91
+-- PHP Version: 5.1.6
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
--- Table structure for table `attribute_values`
+-- Database: `splashuser`
 --
 
-DROP TABLE IF EXISTS `attribute_values`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `attribute_values` (
-  `id` int(10) NOT NULL,
-  `attribute_id` int(10) NOT NULL,
-  `value` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `attribute_id` (`attribute_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `attributes`
 --
 
-DROP TABLE IF EXISTS `attributes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `attributes` (
-  `id` int(10) NOT NULL,
+  `id` int(10) NOT NULL auto_increment,
   `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `charity`
+-- Table structure for table `attributevalues`
 --
 
-DROP TABLE IF EXISTS `charity`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `charity` (
-  `id` int(10) NOT NULL,
+CREATE TABLE `attributevalues` (
+  `id` int(10) NOT NULL auto_increment,
+  `attribute_id` int(10) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `attribute_id` (`attribute_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `charities`
+--
+
+CREATE TABLE `charities` (
+  `id` int(10) NOT NULL auto_increment,
   `name` varchar(255) NOT NULL,
   `address` varchar(1000) NOT NULL,
   `url` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `charities_attributes`
+--
+
+CREATE TABLE `charities_attributes` (
+  `charity_id` int(10) NOT NULL,
+  `attribute_id` int(10) NOT NULL,
+  `attributevalue_id` int(10) NOT NULL,
+  KEY `charity_id` (`charity_id`),
+  KEY `attribute_id` (`attribute_id`),
+  KEY `attributevalue_id` (`attributevalue_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
-  `id` int(10) NOT NULL,
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL auto_increment,
   `fb_id` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `user_attributes`
+-- Table structure for table `users_attributes`
 --
 
-DROP TABLE IF EXISTS `user_attributes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_attributes` (
+CREATE TABLE `users_attributes` (
   `user_id` int(10) NOT NULL,
   `attribute_id` int(10) NOT NULL,
   `value` varchar(255) NOT NULL,
   KEY `user_id` (`user_id`),
   KEY `attribute_id` (`attribute_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `xrf_charity_attribute`
+-- Table structure for table `users_charities`
 --
 
-DROP TABLE IF EXISTS `xrf_charity_attribute`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `xrf_charity_attribute` (
-  `charity_id` int(10) NOT NULL,
-  `attribute_id` int(10) NOT NULL,
-  `attribute_value_id` int(10) NOT NULL,
-  KEY `charity_id` (`charity_id`),
-  KEY `attribute_id` (`attribute_id`),
-  KEY `attribute_value_id` (`attribute_value_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `xrf_user_charity`
---
-
-DROP TABLE IF EXISTS `xrf_user_charity`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `xrf_user_charity` (
+CREATE TABLE `users_charities` (
   `user_id` int(10) NOT NULL,
   `charity_id` int(10) NOT NULL,
   KEY `user_id` (`user_id`),
   KEY `charity_id` (`charity_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2011-06-04 14:19:18
